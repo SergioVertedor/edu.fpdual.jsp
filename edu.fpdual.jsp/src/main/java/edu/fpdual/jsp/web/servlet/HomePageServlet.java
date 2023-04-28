20f00e916d9fee5cd0d9e21c110547c8e974d133
doridc06 <a_dorianaangelica.dacostamagello@iespablopicasso.es>
2023-04-28 14:04:56 +0200

    agregando2

---
+++ edu.fpdual.jsp/src/main/java/edu/fpdual/jsp/web/servlet/HomePageServlet.java
@@ -0,0 +1,16 @@
+package edu.fpdual.jsp.web.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomePageServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/comun/HomePage.jsp").forward(request, response);
+    }
+
+}
