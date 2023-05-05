a7a6ec4bb364d3a1886f25ddbc6c2289ad30b43b
DIR\s.vertedor.beltran <vertedor86@gmail.com>
2023-05-05 13:28:44 +0200

    Arreglado servlet y actualizada dependencias.

--- edu.fpdual.jsp/src/main/java/edu/fpdual/jsp/web/servlet/MiServlet.java
+++ edu.fpdual.jsp/src/main/java/edu/fpdual/jsp/web/servlet/MiServlet.java
@@ -1,25 +1,25 @@
 package edu.fpdual.jsp.web.servlet;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "MiServlet", urlPatterns ={"/mi-servlet"})
+@WebServlet(
+    name = "MiServlet",
+    urlPatterns = {"/mi-servlet2"})
 public class MiServlet extends HttpServlet {
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-					throws
-					ServletException,
-					IOException {
-		PrintWriter writer = resp.getWriter();
-		writer.println("<html>");
-		writer.println("<body>");
-		writer.println("<h2>Mi Servlet! =D</h2>");
-		writer.println("</body>");
-		writer.println("</html>");
-	}
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    PrintWriter writer = resp.getWriter();
+    writer.println("<html>");
+    writer.println("<body>");
+    writer.println("<h2>Mi Servlet! =D</h2>");
+    writer.println("</body>");
+    writer.println("</html>");
+  }
 }
