b2870906f050bb168eb97d04c356239775ce44a5
doridc06 <a_dorianaangelica.dacostamagello@iespablopicasso.es>
2023-05-19 14:53:22 +0200

    agregue lambda

--- edu.fpdual.jsp/src/main/java/edu/fpdual/jsp/web/servlet/MemoriaServlet.java
+++ edu.fpdual.jsp/src/main/java/edu/fpdual/jsp/web/servlet/MemoriaServlet.java
@@ -19,15 +19,20 @@ public class MemoriaServlet extends HttpServlet {
             throws ServletException, IOException {
         String pregunta = request.getParameter("answer");
         int operacion = 32 * 1024 - (5 * 500);
-        String mensaje;
 
-        if (pregunta.equals(String.valueOf(operacion))) {
-            mensaje = "¡Respuesta correcta! Al pendrive le queda " + operacion + " MB de memoria.";
-        } else {
-            mensaje = "Respuesta incorrecta. Inténtalo de nuevo.";
-        }
+        String mensaje = calcularMensaje(pregunta, operacion);
 
         request.setAttribute("mensaje", mensaje);
         request.getRequestDispatcher("/comun/memoria.jsp").forward(request, response);
+    }
+
+    private String calcularMensaje(String pregunta, int operacion) {
+        return (String) ((java.util.function.Function<String, String>) (p) -> {
+            if (p.equals(String.valueOf(operacion))) {
+                return "¡Respuesta correcta! Al pendrive le queda " + operacion + " MB de memoria.";
+            } else {
+                return "Respuesta incorrecta. Inténtalo de nuevo.";
+            }
+        }).apply(pregunta);
     }
 }
